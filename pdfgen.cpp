f1371982d0256142e4390e8656145f9fa339661a
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-12-31 17:12:37 +0700

    Initial commit

---
+++ pdfgen.cpp
@@ -0,0 +1,22 @@
+// pdfgen.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//
+//#define _CRT_SECURE_NO_DEPRECATE
+//
+//#include <stdio.h>
+//#include "pch.h"
+//#include <iostream>
+//#include "pdfgen.h"
+
+////int main() {
+////	//struct pdf_doc *pdf = pdf_create(PDF_A4_WIDTH, PDF_A4_HEIGHT);
+////	//pdf_set_font(pdf, "Times-Roman");
+////	//pdf_append_page(pdf);
+////	//pdf_add_text(pdf, NULL, "This is text", 12, 50, 20, PDF_BLACK);
+////	//pdf_add_line(pdf, NULL, 50, 24, 150, 24, 3, PDF_BLACK);
+////	//pdf_save(pdf, "output.pdf");
+////	//pdf_destroy(pdf);
+////
+////	return 0;
+////}
